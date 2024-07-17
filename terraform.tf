a84202298cd9edbcbf1295fea7a18010b678e8cc
Joshua <Joshua@mcs.local>
2024-07-17 13:50:21 -0400

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -2,21 +2,20 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "MCS_Tech_Services"
 
     workspaces {
       name = "learn-terraform"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.31.0"
+      version = "~> 5.58.0"
     }
   }
-
   required_version = "~> 1.2"
 }
